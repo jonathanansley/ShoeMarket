e260d7b69b9c4abfeb5468ac8b022c49d323dde7
Ezha Randall <ezha.randall@gmail.com>
2017-09-09 13:32:35 -0400

    edited charts to reflect better data

--- public/js/charts-custom.js
+++ public/js/charts-custom.js
@@ -15,10 +15,10 @@ $(document).ready(function () {
     var lineChartExample = new Chart(LINECHARTEXMPLE, {
         type: 'line',
         data: {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"],
             datasets: [
                 {
-                    label: "Data Set One",
+                    label: "Profits in USD Per Month",
                     fill: true,
                     lineTension: 0.3,
                     backgroundColor: "rgba(51, 179, 90, 0.38)",
@@ -37,32 +37,32 @@ $(document).ready(function () {
                     pointHoverBorderWidth: 2,
                     pointRadius: 1,
                     pointHitRadius: 10,
-                    data: [50, 20, 40, 31, 32, 22, 10],
+                    data: [500, 200, 800, 100, 1100, 590, 600, 800, 400, 1200, 1300, 1350],
                     spanGaps: false
                 },
-                {
-                    label: "Data Set Two",
-                    fill: true,
-                    lineTension: 0.3,
-                    backgroundColor: "rgba(75,192,192,0.4)",
-                    borderColor: "rgba(75,192,192,1)",
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    borderWidth: 1,
-                    pointBorderColor: "rgba(75,192,192,1)",
-                    pointBackgroundColor: "#fff",
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                    pointHoverBorderColor: "rgba(220,220,220,1)",
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 1,
-                    pointHitRadius: 10,
-                    data: [65, 59, 30, 81, 56, 55, 40],
-                    spanGaps: false
-                }
+                // {
+                //     label: "Data Set Two",
+                //     fill: true,
+                //     lineTension: 0.3,
+                //     backgroundColor: "rgba(75,192,192,0.4)",
+                //     borderColor: "rgba(75,192,192,1)",
+                //     borderCapStyle: 'butt',
+                //     borderDash: [],
+                //     borderDashOffset: 0.0,
+                //     borderJoinStyle: 'miter',
+                //     borderWidth: 1,
+                //     pointBorderColor: "rgba(75,192,192,1)",
+                //     pointBackgroundColor: "#fff",
+                //     pointBorderWidth: 1,
+                //     pointHoverRadius: 5,
+                //     pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                //     pointHoverBorderColor: "rgba(220,220,220,1)",
+                //     pointHoverBorderWidth: 2,
+                //     pointRadius: 1,
+                //     pointHitRadius: 10,
+                //     data: [65, 59, 30, 81, 56, 55, 40],
+                //     spanGaps: false
+                // }
             ]
         }
     });
@@ -71,14 +71,15 @@ $(document).ready(function () {
         type: 'doughnut',
         data: {
             labels: [
-                "First",
-                "Second",
-                "Third"
+                "Jordans",
+                "LeBron",
+                "Kyrie",
+                "KD"
             ],
             datasets: [
                 {
-                    data: [300, 50, 100],
-                    borderWidth: [1, 1, 1],
+                    data: [300, 50, 100, 20],
+                    borderWidth: [1, 1, 1, 1],
                     backgroundColor: [
                         brandPrimary,
                         "rgba(75,192,192,1)",
@@ -100,7 +101,7 @@ $(document).ready(function () {
     var barChartExample = new Chart(BARCHARTEXMPLE, {
         type: 'bar',
         data: {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"],
             datasets: [
                 {
                     label: "Data Set 1",
@@ -111,7 +112,12 @@ $(document).ready(function () {
                         'rgba(51, 179, 90, 0.6)',
                         'rgba(51, 179, 90, 0.6)',
                         'rgba(51, 179, 90, 0.6)',
-                        'rgba(51, 179, 90, 0.6)'
+                        'rgba(51, 179, 90, 0.6)',
+                        'rgba(51, 179, 90, 0.6)',
+                        'rgba(51, 179, 90, 0.6)',
+                        'rgba(51, 179, 90, 0.6)',
+                        'rgba(51, 179, 90, 0.6)',
+                        'rgba(51, 179, 90, 0.6)',
                     ],
                     borderColor: [
                         'rgba(51, 179, 90, 1)',
@@ -120,34 +126,39 @@ $(document).ready(function () {
                         'rgba(51, 179, 90, 1)',
                         'rgba(51, 179, 90, 1)',
                         'rgba(51, 179, 90, 1)',
-                        'rgba(51, 179, 90, 1)'
+                        'rgba(51, 179, 90, 1)',
+                        'rgba(51, 179, 90, 1)',
+                        'rgba(51, 179, 90, 1)',
+                        'rgba(51, 179, 90, 1)',
+                        'rgba(51, 179, 90, 1)',
+                        'rgba(51, 179, 90, 1)',
                     ],
                     borderWidth: 1,
-                    data: [65, 59, 80, 81, 56, 55, 40],
+                    data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56],
                 },
-                {
-                    label: "Data Set 2",
-                    backgroundColor: [
-                        'rgba(203, 203, 203, 0.6)',
-                        'rgba(203, 203, 203, 0.6)',
-                        'rgba(203, 203, 203, 0.6)',
-                        'rgba(203, 203, 203, 0.6)',
-                        'rgba(203, 203, 203, 0.6)',
-                        'rgba(203, 203, 203, 0.6)',
-                        'rgba(203, 203, 203, 0.6)'
-                    ],
-                    borderColor: [
-                        'rgba(203, 203, 203, 1)',
-                        'rgba(203, 203, 203, 1)',
-                        'rgba(203, 203, 203, 1)',
-                        'rgba(203, 203, 203, 1)',
-                        'rgba(203, 203, 203, 1)',
-                        'rgba(203, 203, 203, 1)',
-                        'rgba(203, 203, 203, 1)'
-                    ],
-                    borderWidth: 1,
-                    data: [35, 40, 60, 47, 88, 27, 30],
-                }
+                // {
+                //     label: "Data Set 2",
+                //     backgroundColor: [
+                //         'rgba(203, 203, 203, 0.6)',
+                //         'rgba(203, 203, 203, 0.6)',
+                //         'rgba(203, 203, 203, 0.6)',
+                //         'rgba(203, 203, 203, 0.6)',
+                //         'rgba(203, 203, 203, 0.6)',
+                //         'rgba(203, 203, 203, 0.6)',
+                //         'rgba(203, 203, 203, 0.6)'
+                //     ],
+                //     borderColor: [
+                //         'rgba(203, 203, 203, 1)',
+                //         'rgba(203, 203, 203, 1)',
+                //         'rgba(203, 203, 203, 1)',
+                //         'rgba(203, 203, 203, 1)',
+                //         'rgba(203, 203, 203, 1)',
+                //         'rgba(203, 203, 203, 1)',
+                //         'rgba(203, 203, 203, 1)'
+                //     ],
+                //     borderWidth: 1,
+                //     data: [35, 40, 60, 47, 88, 27, 30],
+                // }
             ]
         }
     });
